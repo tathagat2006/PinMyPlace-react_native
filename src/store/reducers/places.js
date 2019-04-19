f35556db5051daa5ffbb2068ebd062cb4fc08b0a
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-19 22:48:55 +0530

    add place reducer made
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,3 +1,10 @@
+import {
+  ADD_PLACE,
+  SELECT_PLACE,
+  DELETE_PLACE,
+  DESELECT_PLACE
+} from "../actions/actionTypes";
+
 const initialState = {
   places: [],
   selectedPlace: null
@@ -5,6 +12,18 @@ const initialState = {
 
 const reducers = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_PLACE:
+      return {
+        ...state,
+        places: state.places.concat({
+          key: Math.random(),
+          name: action.placeName,
+          image: {
+            uri:
+              "https://i.pinimg.com/originals/70/fc/0c/70fc0c9846423ccf3ff8506fd9268233.jpg"
+          }
+        })
+      };
     default:
       return state;
   }
