e329e4913fb5494544cc6f7095d10907e4ab718b
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-19 22:43:10 +0530

    setting up action creators
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initialState = {
+  places: [],
+  selectedPlace: null
+};
+
+const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
