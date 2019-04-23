d73f27b2e216a55fd6fc9ec38b2afd0aef19f1f6
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-23 14:07:46 +0530

    making delete button custom using ionicons
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,6 +1,14 @@
 import React from "react";
-import { Modal, View, Image, Text, Button, StyleSheet } from "react-native";
-
+import {
+  Modal,
+  View,
+  Image,
+  Text,
+  Button,
+  StyleSheet,
+  TouchableOpacity
+} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 const placeDetail = props => {
   let modalContent = null;
 
@@ -24,7 +32,12 @@ const placeDetail = props => {
       <View style={styles.modalContainer}>
         {modalContent}
         <View>
-          <Button title="Delete" color="red" onPress={props.onItemDeleted} />
+          {/* <Button title="Delete" color="red" onPress={props.onItemDeleted} /> */}
+          <TouchableOpacity onPress={props.onItemDeleted}>
+            <View style={styles.deleteButton}>
+              <Icon size={30} name="ios-trash" color="red" />
+            </View>
+          </TouchableOpacity>
           <Button title="Close" onPress={props.onModalClosed} />
         </View>
       </View>
@@ -44,6 +57,9 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 28
+  },
+  deleteButton: {
+    alignItems: "center"
   }
 });
 
