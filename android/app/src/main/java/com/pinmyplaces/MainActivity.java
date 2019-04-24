d2d1bdf01189d961d79213b2255f2e762d1de57a
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-25 02:08:53 +0530

    configuring react-native-navigation
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- android/app/src/main/java/com/pinmyplaces/MainActivity.java
+++ android/app/src/main/java/com/pinmyplaces/MainActivity.java
@@ -1,15 +1,17 @@
 package com.pinmyplaces;
 
 import com.facebook.react.ReactActivity;
+import com.reactnativenavigation.controllers.SplashActivity;
 
-public class MainActivity extends ReactActivity {
+public class MainActivity extends SplashActivity {
 
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
      */
-    @Override
-    protected String getMainComponentName() {
-        return "PinMyPlaces";
-    }
+//     @Override
+//     protected String getMainComponentName() {
+//         return "PinMyPlaces";
+//     }
+// }
 }
--- android/app/src/main/java/com/pinmyplaces/MainApplication.java
+++ android/app/src/main/java/com/pinmyplaces/MainApplication.java
@@ -7,41 +7,69 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
+// public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+//   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+//     @Override
+//     public boolean getUseDeveloperSupport() {
+//       return BuildConfig.DEBUG;
+//     }
 
-    @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-          new VectorIconsPackage()
-      );
-    }
+//     @Override
+//     protected List<ReactPackage> getPackages() {
+//       return Arrays.<ReactPackage>asList(
+//           new MainReactPackage(),
+//           new VectorIconsPackage()
+//       );
+//     }
 
-    @Override
-    protected String getJSMainModuleName() {
-      return "index";
-    }
-  };
+//     @Override
+//     protected String getJSMainModuleName() {
+//       return "index";
+//     }
+//   };
 
+//   @Override
+//   public ReactNativeHost getReactNativeHost() {
+//     return mReactNativeHost;
+//   }
+
+//   @Override
+//   public void onCreate() {
+//     super.onCreate();
+//     SoLoader.init(this, /* native exopackage */ false);
+//   }
+// }
+
+public class MainApplication extends NavigationApplication {
+ 
   @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
   }
-
+ 
+  protected List<ReactPackage> getPackages() {
+    // Add additional packages you require here
+    // No need to add RnnPackage and MainReactPackage
+    return Arrays.<ReactPackage>asList(
+      new VectorIconsPackage()
+    );
+  }
+ 
   @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return getPackages();
+  }
+ 
+  @Override
+  public String getJSMainModuleName() {
+    return "index";
   }
 }
