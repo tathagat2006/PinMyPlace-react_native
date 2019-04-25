c28e14a2699946890360a5e52f77f5a542401dea
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-25 17:31:45 +0530

    find place screen made
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ src/screens/FindPLace/FindPlace.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import { View, Text } from "react-native";
+
+class FindPlaceScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>On Share Place Screen</Text>
+      </View>
+    );
+  }
+}
+export default FindPlaceScreen;
