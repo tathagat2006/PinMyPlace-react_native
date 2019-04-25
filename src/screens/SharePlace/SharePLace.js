1f7b3ce91749a8bc9cbdd5fc6050a7c3f0dce1ee
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-25 17:31:00 +0530

    share place screen made
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ src/screens/SharePlace/SharePLace.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+import { View, Text } from "react-native";
+
+class SharePlaceScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text>On Share Place Screen</Text>
+      </View>
+    );
+  }
+}
+export default SharePlaceScreen;
