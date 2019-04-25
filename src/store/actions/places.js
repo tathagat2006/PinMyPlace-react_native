d82cf4f43b0215d3e99cc4184226d2d0d68b5467
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-26 01:53:30 +0530

    updating redux
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,9 +1,4 @@
-import {
-  ADD_PLACE,
-  DELETE_PLACE,
-  SELECT_PLACE,
-  DESELECT_PLACE
-} from "./actionTypes";
+import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = placeName => {
   return {
@@ -18,15 +13,15 @@ export const deletePlace = () => {
   };
 };
 
-export const selectPlace = key => {
-  return {
-    type: SELECT_PLACE,
-    placeKey: key
-  };
-};
+// export const selectPlace = key => {
+//   return {
+//     type: SELECT_PLACE,
+//     placeKey: key
+//   };
+// };
 
-export const deselectPlace = () => {
-  return {
-    type: DESELECT_PLACE
-  };
-};
+// export const deselectPlace = () => {
+//   return {
+//     type: DESELECT_PLACE
+//   };
+// };
--- src/store/reducers/places.js
+++ src/store/reducers/places.js
@@ -1,13 +1,8 @@
-import {
-  ADD_PLACE,
-  SELECT_PLACE,
-  DELETE_PLACE,
-  DESELECT_PLACE
-} from "../actions/actionTypes";
+import { ADD_PLACE, DELETE_PLACE } from "../actions/actionTypes";
 
 const initialState = {
-  places: [],
-  selectedPlace: null
+  places: []
+  // selectedPlace: null
 };
 
 const reducers = (state = initialState, action) => {
@@ -29,20 +24,8 @@ const reducers = (state = initialState, action) => {
         ...state,
         places: state.places.filter(place => {
           return place.key !== state.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    case SELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: state.places.find(place => {
-          return place.key === action.placeKey;
         })
-      };
-    case DESELECT_PLACE:
-      return {
-        ...state,
-        selectedPlace: null
+        // selectedPlace: null
       };
     default:
       return state;
