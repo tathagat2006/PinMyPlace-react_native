fe5293a4dcbbf0f76032c1d93a89f1b1e708026f
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-26 22:31:10 +0530

    styling using flexbox
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
+import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import startMainTabs from "../MainTabs/startMainTabs";
 
 class AuthScreen extends Component {
@@ -8,12 +8,25 @@ class AuthScreen extends Component {
   };
   render() {
     return (
-      <View>
-        <Text>Auth Screen</Text>
-        <Button title="Login" onPress={this.loginHandler} />
+      <View style={styles.container}>
+        <Text>Please Log In</Text>
+        <Button title="Switch To Log In" />
+        <TextInput placeholder="Your Email Address" />
+        <TextInput placeholder="Password" />
+        <TextInput placeholder="Confirm Password" />
+        <Button title="Submit" onPress={this.loginHandler} />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: "red",
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 export default AuthScreen;
