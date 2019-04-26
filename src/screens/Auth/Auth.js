6d646612c2c2288fce5d80e93eff78cacf6bf608
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-26 23:33:20 +0530

    styling text
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import startMainTabs from "../MainTabs/startMainTabs";
 import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
+import HeadingText from "../../components/UI/HeadingText/HeadingText";
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -10,7 +11,7 @@ class AuthScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Please Log In</Text>
+        <HeadingText>Please Log In</HeadingText>
         <Button title="Switch To Log In" />
         <View style={styles.inputContainer}>
           <DefaultInput placeholder="Your Email Address" style={styles.input} />
