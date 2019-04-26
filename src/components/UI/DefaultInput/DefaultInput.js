637b7b6536652c3e9afc7484453c04371c23345a
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-26 23:01:24 +0530

    global styling using custom inputs
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

---
+++ src/components/UI/DefaultInput/DefaultInput.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { TextInput, StyleSheet } from "react-native";
+
+const defaultInput = props => {
+  return (
+    <TextInput
+      placeholder={props.placeholder}
+      style={styles.input}
+      underlineColorAndroid="transparent"
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    margin: 8
+  }
+});
+
+export default defaultInput;
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { View, Text, Button, TextInput, StyleSheet } from "react-native";
 import startMainTabs from "../MainTabs/startMainTabs";
+import DefaultInput from "../../components/UI/DefaultInput/DefaultInput";
 
 class AuthScreen extends Component {
   loginHandler = () => {
@@ -12,9 +13,9 @@ class AuthScreen extends Component {
         <Text>Please Log In</Text>
         <Button title="Switch To Log In" />
         <View style={styles.inputContainer}>
-          <TextInput placeholder="Your Email Address" style={styles.input} />
-          <TextInput placeholder="Password" style={styles.input} />
-          <TextInput placeholder="Confirm Password" style={styles.input} />
+          <DefaultInput placeholder="Your Email Address" />
+          <DefaultInput placeholder="Password" />
+          <DefaultInput placeholder="Confirm Password" />
         </View>
         <Button title="Submit" onPress={this.loginHandler} />
       </View>
@@ -33,7 +34,11 @@ const styles = StyleSheet.create({
     width: "80%"
   },
   input: {
-    width: "100%"
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#eee",
+    padding: 5,
+    margin: 8
   }
 });
 
