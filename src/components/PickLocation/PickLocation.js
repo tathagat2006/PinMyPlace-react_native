26c9972e00f533c77231bf28adb61e749c2aff86
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-28 02:35:10 +0530

    locate me btn working
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/components/PickLocation/PickLocation.js
+++ src/components/PickLocation/PickLocation.js
@@ -33,6 +33,24 @@ class PickLocation extends Component {
       };
     });
   };
+  getLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(
+      pos => {
+        const coordsEvent = {
+          nativeEvent: {
+            coordinate: {
+              latitude: pos.coords.latitude,
+              longitude: pos.coords.longitude
+            }
+          }
+        };
+        this.pickLocationHandler(coordsEvent);
+      },
+      err => {
+        alert("Fetching the position failed. Please select one manually!");
+      }
+    );
+  };
   render() {
     let marker = null;
 
@@ -50,7 +68,7 @@ class PickLocation extends Component {
           {marker}
         </MapView>
         <View style={styles.button}>
-          <Button title="Locate Me!" onPress={() => alert("Pick Location!")} />
+          <Button title="Locate Me!" onPress={this.getLocationHandler} />
         </View>
       </View>
     );
