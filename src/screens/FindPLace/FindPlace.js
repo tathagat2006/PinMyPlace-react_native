63aaafa4649e9f87703f687029409f8e9b976774
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-27 19:08:57 +0530

    preparing the component for animation
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/screens/FindPLace/FindPlace.js
+++ src/screens/FindPLace/FindPlace.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import { connect } from "react-redux";
 
 import PlaceList from "../../components/PlaceList/PlaceList";
@@ -8,6 +8,11 @@ class FindPlaceScreen extends Component {
   static navigatorStyle = {
     navBarButtonColor: "orange"
   };
+
+  state = {
+    placesLoaded: false
+  };
+
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent);
@@ -22,6 +27,11 @@ class FindPlaceScreen extends Component {
       }
     }
   };
+  placesSearchHandler = () => {
+    this.setState({
+      placesLoaded: true
+    });
+  };
   itemSelectedHandler = key => {
     const selectedPlace = this.props.places.find(place => {
       return place.key === key;
@@ -35,16 +45,47 @@ class FindPlaceScreen extends Component {
     });
   };
   render() {
-    return (
-      <View>
+    let content = (
+      <TouchableOpacity onPress={this.placesSearchHandler}>
+        <View style={styles.searchButton}>
+          <Text style={styles.searchButtonText}>Find Places</Text>
+        </View>
+      </TouchableOpacity>
+    );
+    if (this.state.placesLoaded) {
+      content = (
         <PlaceList
           places={this.props.places}
           onItemSelected={this.itemSelectedHandler}
         />
+      );
+    }
+    return (
+      <View style={this.state.placesLoaded ? null : styles.buttonContainer}>
+        {content}
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  searchButton: {
+    borderColor: "orange",
+    borderWidth: 3,
+    borderRadius: 50,
+    padding: 20
+  },
+  searchButtonText: {
+    color: "orange",
+    fontWeight: "bold",
+    fontSize: 26
+  }
+});
 
 const mapStateToProps = state => {
   return {
