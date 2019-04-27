e0ac8221467cb0e597e9ee33317a05845f65b532
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-27 21:30:02 +0530

    writing validation logic
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -14,6 +14,7 @@ import HeadingText from "../../components/UI/HeadingText/HeadingText";
 import MainText from "../../components/UI/MainText/MainText";
 import background from "../../assets/image1.jpg";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
+import validate from "../../utility/validation";
 
 class AuthScreen extends Component {
   state = {
@@ -71,13 +72,44 @@ class AuthScreen extends Component {
   };
 
   updateInputState = (key, value) => {
+    let connectedValue = {};
+    if (this.state.controls[key].validationRules.equalTo) {
+      const equalControl = this.state.controls[key].validationRules.equalTo;
+      const equalValue = this.state.controls[equalControl].value;
+      connectedValue = {
+        ...connectedValue,
+        equalTo: equalValue
+      };
+    }
+    if (key === "password") {
+      connectedValue = {
+        ...connectedValue,
+        equalTo: value
+      };
+    }
     this.setState(prevState => {
       return {
         controls: {
           ...prevState.controls,
+          confirmPassword: {
+            ...prevState.controls.confirmPassword,
+            valid:
+              key === "password"
+                ? validate(
+                    prevState.controls.confirmPassword.value,
+                    prevState.controls.confirmPassword.validationRules,
+                    connectedValue
+                  )
+                : prevState.controls.confirmPassword.valid
+          },
           [key]: {
             ...prevState.controls[key],
-            value: value
+            value: value,
+            valid: validate(
+              value,
+              prevState.controls[key].validationRules,
+              connectedValue
+            )
           }
         }
       };
---
+++ src/utility/validation.js
@@ -0,0 +1,35 @@
+const validate = (val, rules, connectedValue) => {
+  let isValid = true;
+  for (let rule in rules) {
+    switch (rule) {
+      case "isEmail":
+        isValid = isValid && emailValidator(val);
+        break;
+      case "minLength":
+        isValid = isValid && minLengthValidator(val, rules[rule]);
+        break;
+      case "equalTo":
+        isValid = isValid && equalToValidator(val, connectedValue[rule]);
+        break;
+      default:
+        isValid = true;
+    }
+  }
+  return isValid;
+};
+
+const emailValidator = val => {
+  return /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(
+    val
+  );
+};
+
+const minLengthValidator = (val, minLength) => {
+  return val.length >= minLength;
+};
+
+const equalToValidator = (val, checkVal) => {
+  return val === checkVal;
+};
+
+export default validate;
