ac8f1de627cf4091df34a7e5bae1f7e5bfad6717
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-27 18:41:31 +0530

    adjusting styles dynamically
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -16,6 +16,29 @@ import background from "../../assets/image1.jpg";
 import ButtonWithBackground from "../../components/UI/ButtonWithBackground/ButtonWithBackground";
 
 class AuthScreen extends Component {
+  state = {
+    respStyles: {
+      pwContainerDirection: "column",
+      pwContainerJustifyContent: "flex-start",
+      pwWrapperWidth: "100%"
+    }
+  };
+  constructor(props) {
+    super(props);
+    Dimensions.addEventListener("change", dims => {
+      this.setState({
+        respStyles: {
+          pwContainerDirection:
+            Dimensions.get("window").height > 500 ? "column" : "row",
+          pwContainerJustifyContent:
+            Dimensions.get("window").height > 500
+              ? "flex-start"
+              : "space-between",
+          pwWrapperWidth: Dimensions.get("window").height > 500 ? "100%" : "45%"
+        }
+      });
+    });
+  }
   loginHandler = () => {
     startMainTabs();
   };
@@ -41,11 +64,16 @@ class AuthScreen extends Component {
               placeholder="Your Email Address"
               style={styles.input}
             />
-            <View style={styles.passwordContainer}>
-              <View style={styles.passwordWrapper}>
+            <View
+              style={{
+                flexDirection: this.state.respStyles.pwContainerDirection,
+                justifyContent: this.state.respStyles.pwContainerJustifyContent
+              }}
+            >
+              <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
                 <DefaultInput placeholder="Password" style={styles.input} />
               </View>
-              <View style={styles.passwordWrapper}>
+              <View style={{ width: this.state.respStyles.pwWrapperWidth }}>
                 <DefaultInput
                   placeholder="Confirm Password"
                   style={styles.input}
