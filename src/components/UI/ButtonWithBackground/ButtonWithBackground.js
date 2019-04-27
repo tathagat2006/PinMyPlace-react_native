e4f504352c21fc916fba9b37ecbb22883f314aa5
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-04-27 16:59:56 +0530

    using platform api to load diff btns
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- src/components/UI/ButtonWithBackground/ButtonWithBackground.js
+++ src/components/UI/ButtonWithBackground/ButtonWithBackground.js
@@ -1,12 +1,28 @@
 import React from "react";
-import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
-const buttonWithBackGround = props => (
-  <TouchableOpacity onPress={props.onPress}>
+import {
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  Text,
+  View,
+  StyleSheet,
+  Platform
+} from "react-native";
+
+const buttonWithBackGround = props => {
+  const content = (
     <View style={[styles.button, { backgroundColor: props.color }]}>
       <Text>{props.children}</Text>
     </View>
-  </TouchableOpacity>
-);
+  );
+  if (Platform.OS === "android") {
+    return (
+      <TouchableNativeFeedback onPress={props.onPress}>
+        {content}
+      </TouchableNativeFeedback>
+    );
+  }
+  return <TouchableOpacity onPress={props.onPress}>{content}</TouchableOpacity>;
+};
 
 const styles = StyleSheet.create({
   button: {
