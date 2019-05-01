72b6815dda1c068ed424c8a3e8c9da02055024e1
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-05-02 03:33:24 +0530

    storing images
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,57 @@
 const functions = require("firebase-functions");
+const cors = require("cors")({ origin: true });
+const fs = require("fs");
+const UUID = require("uuid-v4");
+
+const gcconfig = {
+  projectId: "pinmyplaces.json",
+  keyFilename: "pinmyplaces"
+};
+
+const { Storage } = require("@google-cloud/storage");
+const gcs = new Storage(gcconfig);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
+  return cors(request, response, () => {
+    const body = JSON.parse(request.body);
+    fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
+      console.log(err);
+      return response.status(500).json({ error: err });
+    });
+    const bucket = gcs.bucket("gs://pinmyplaces.appspot.com");
+    const uuid = UUID();
+
+    return bucket.upload(
+      "/tmp/uploaded-image.jpg",
+      {
+        uploadType: "media",
+        destination: "/places/" + uuid + ".jpg",
+        metadata: {
+          metadata: {
+            contentType: "image/jpeg",
+            firebaseStorageDownloadTokens: uuid
+          }
+        }
+      },
+      (err, file) => {
+        if (!err) {
+          return response.status(201).json({
+            imageUrl:
+              "https://firebasestorage.googleapis.com/v0/b/" +
+              bucket.name +
+              "/o/" +
+              encodeURIComponent(file.name) +
+              "?alt=media&token=" +
+              uuid
+          });
+        } else {
+          console.log(err);
+          return response.status(500).json({ error: err });
+        }
+      }
+    );
+  });
 });
--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -2,17 +2,26 @@ import { ADD_PLACE, DELETE_PLACE } from "./actionTypes";
 
 export const addPlace = (placeName, location, image) => {
   return dispatch => {
-    const placeData = {
-      name: placeName,
-      location: location
-    };
-    fetch("https://pinmyplaces.firebaseio.com/places.json", {
+    fetch("https://us-central1-pinmyplaces.cloudfunctions.net/storeImage", {
       method: "POST",
-      body: JSON.stringify(placeData)
-    })
-      .catch(err => {
-        console.log(err);
+      body: JSON.stringify({
+        image: image.base64
       })
+    })
+      .catch(err => console.log(err))
+      .then(res => res.json())
+      .then(parsedRes => {
+        const placeData = {
+          name: placeName,
+          location: location,
+          image: parsedRes.imageUrl
+        };
+        return fetch("https://pinmyplaces.firebaseio.com/places.json", {
+          method: "POST",
+          body: JSON.stringify(placeData)
+        });
+      })
+      .catch(err => console.log(err))
       .then(res => res.json())
       .then(parsedRes => {
         console.log(parsedRes);
