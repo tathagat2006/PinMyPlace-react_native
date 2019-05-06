e4fedb37c7e300ac5f1b45eba0805a167de76a73
Tathagat Thapliyal <tathagat.thapliyal@gmail.com>
2019-05-07 01:06:45 +0530

    protecting the firebase cloud function
    Signed-off-by: Tathagat Thapliyal <tathagat.thapliyal@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 const cors = require("cors")({ origin: true });
 const fs = require("fs");
 const UUID = require("uuid-v4");
+const admin = require("firebase-admin");
 
 const gcconfig = {
   projectId: "pinmyplaces",
@@ -11,47 +12,73 @@ const gcconfig = {
 const { Storage } = require("@google-cloud/storage");
 const gcs = new Storage(gcconfig);
 
+admin.initializeApp({
+  credential: admin.credential.cert(require("./pinmyplaces.json"))
+});
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
 exports.storeImage = functions.https.onRequest((request, response) => {
   return cors(request, response, () => {
-    const body = JSON.parse(request.body);
-    fs.writeFileSync("/tmp/uploaded-image.jpg", body.image, "base64", err => {
-      console.log(err);
-      return response.status(500).json({ error: err });
-    });
-    const bucket = gcs.bucket("pinmyplaces.appspot.com");
-    const uuid = UUID();
-
-    return bucket.upload(
-      "/tmp/uploaded-image.jpg",
-      {
-        uploadType: "media",
-        destination: "/places/" + uuid + ".jpg",
-        metadata: {
-          metadata: {
-            contentType: "image/jpeg",
-            firebaseStorageDownloadTokens: uuid
+    if (
+      !request.headers.authorization ||
+      !request.authorization.startsWith("Bearer ")
+    ) {
+      console.log("No token found!");
+      response.status(403).json({ error: "Unauthorized" });
+      return;
+    }
+    let idToken = request.headers.authorization.split("Bearer ")[1];
+    admin
+      .auth()
+      .verifyIdToken(idToken)
+      .then(decodedToken => {
+        const body = JSON.parse(request.body);
+        fs.writeFileSync(
+          "/tmp/uploaded-image.jpg",
+          body.image,
+          "base64",
+          err => {
+            console.log(err);
+            return response.status(500).json({ error: err });
           }
-        }
-      },
-      (err, file) => {
-        if (!err) {
-          return response.status(201).json({
-            imageUrl:
-              "https://firebasestorage.googleapis.com/v0/b/" +
-              bucket.name +
-              "/o/" +
-              encodeURIComponent(file.name) +
-              "?alt=media&token=" +
-              uuid
-          });
-        } else {
-          console.log(err);
-          return response.status(500).json({ error: err });
-        }
-      }
-    );
+        );
+        const bucket = gcs.bucket("pinmyplaces.appspot.com");
+        const uuid = UUID();
+
+        return bucket.upload(
+          "/tmp/uploaded-image.jpg",
+          {
+            uploadType: "media",
+            destination: "/places/" + uuid + ".jpg",
+            metadata: {
+              metadata: {
+                contentType: "image/jpeg",
+                firebaseStorageDownloadTokens: uuid
+              }
+            }
+          },
+          (err, file) => {
+            if (!err) {
+              return response.status(201).json({
+                imageUrl:
+                  "https://firebasestorage.googleapis.com/v0/b/" +
+                  bucket.name +
+                  "/o/" +
+                  encodeURIComponent(file.name) +
+                  "?alt=media&token=" +
+                  uuid
+              });
+            } else {
+              console.log(err);
+              return response.status(500).json({ error: err });
+            }
+          }
+        );
+      })
+      .catch(error => {
+        response.status(403).json({ error: "Unauthorized" });
+      });
   });
 });
